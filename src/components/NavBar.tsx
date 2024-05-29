cc87089a3c7b0fdcde9b4df27f621a16ec679d12
Francine Nogueira <francine.pns@gmail.com>
2024-05-29 18:33:32 -0300

    Change font color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto">
-        <h1 className="text-2xl font-bold">Yu-Gi-Oh Card Browser</h1>
+        <h1 className="text-2xl font-bold text-white">Yu-Gi-Oh Card Browser</h1>
       </div>
     </nav>
   );
