26538add99da7dbfedfd8e732f57d2c5e06e1a0f
Francine Nogueira <francine.pns@gmail.com>
2024-05-30 21:37:50 -0300

    Create route

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto">
-        <h1 className="text-2xl font-bold text-white">Yu-Gi-Oh Card Browser</h1>
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-2xl font-bold text-white">
+          Yu-Gi-Oh Card Browser
+        </Link>
+        <Link to="/favorites" className="text-white">
+          Favorites
+        </Link>
       </div>
     </nav>
   );
